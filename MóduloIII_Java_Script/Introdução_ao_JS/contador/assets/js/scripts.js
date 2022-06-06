5d908d470e748cefcb4852f1b99a10c7476c845c
danielafarina <danielafarina@gmail.com>
2022-06-06 20:51:20 -0300

    tentando fazer o exercício do contador - Introdução ao JS - com addEventListener

--- MóduloIII_Java_Script/Introdução_ao_JS/contador/assets/js/scripts.js
+++ MóduloIII_Java_Script/Introdução_ao_JS/contador/assets/js/scripts.js
@@ -1,13 +1,38 @@
+// Fazendo com document.getElementById:
+
+// var numeroAtualWrapper = document.getElementById("numeroAtual");
+
+// var numeroAtual = 0;
+
+// function increment() {
+//     numeroAtual = numeroAtual + 1;
+//     numeroAtualWrapper.innerHTML = numeroAtual;
+// }
+
+// function decrement() {
+//     numeroAtual = numeroAtual - 1;
+//     numeroAtualWrapper.innerHTML = numeroAtual;
+// }
+
+// Fazendo com addEventListener:
+
+const botaoMais = document.getElementById("adicionar");
+const botaoMenos = document.getElementById("subtrair");
+
+botaoMais.addEventListener("click", increment);
+
+botaoMenos.addEventListener("click", decrement);
+
 var numeroAtualWrapper = document.getElementById("numeroAtual");
 
 var numeroAtual = 0;
 
 function increment() {
-    numeroAtual = numeroAtual + 1;
-    numeroAtualWrapper.innerHTML = numeroAtual;
+        numeroAtual += 1; // primeiro vc faz a conta com a variável auxiliar
+        numeroAtualWrapper.innerHTML = numeroAtual; // depois vc coloca a variável auxiliar no innerHTML
 }
-
+    
 function decrement() {
-    numeroAtual = numeroAtual - 1;
-    numeroAtualWrapper.innerHTML = numeroAtual;
-}
+        numeroAtual -= 1;
+        numeroAtualWrapper.innerHTML = numeroAtual;
+    }
