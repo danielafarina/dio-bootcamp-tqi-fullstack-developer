74a07a77a7346361610c920c03b2c270fe624427
danielafarina <danielafarina@gmail.com>
2022-06-18 19:26:59 -0300

    alterando o README.md

--- MóduloIV_ReactJS/Introdução_ReactJS/app/my-app/src/Button.jsx
+++ MóduloIV_ReactJS/Introdução_ReactJS/app/my-app/src/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Button(props) { // a criação de componentes deve ser o mais pura possível, sem estado, sem vários elementos acompanhando o mesmo, pois eles irão ser reutilizados em outros momentos.
-    const { name, onClick } = props // o mesmo que fazer const name = props.name, pois estou fazendo a desestruturação do objeto
+    const { name, onClick } = props // o mesmo que fazer const name = props.name, pois estou fazendo a desestruturação do objeto (para relembrar o que é isso, vá ao final deste código)
 
     return (
         <button onClick={onClick}>{name}</button>
@@ -10,3 +10,27 @@ function Button(props) { // a criação de componentes deve ser o mais pura poss
 }
 
 export default Button
+
+
+// ATIVIDADE 1
+// const ALUNOS = [
+//     { nome: "Dani", nota: 10, turma: "2C" },
+//     { nome: "Márcio", nota: 7, turma: "2A" },
+//     { nome: "Tiago", nota: 5, turma: "2B" },
+//     { nome: "Hudson", nota: 2, turma: "2D"}
+// ];
+
+// function alunosAprovados(arr, media) {
+//     let aprovados = [];
+//     for(let i = 0; i < arr.length; i++) {
+
+//         const {nota, nome} = arr[i]; // object destructuring: pego só as propriedades que eu quiser.
+
+//         if(nota >= media) {
+//             aprovados.push(nome);
+//         }
+//     }
+//     return aprovados;
+// }
+
+// console.log(alunosAprovados(ALUNOS, 6));
